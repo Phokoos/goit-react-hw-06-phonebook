eb9337f6f63f2258786f91da6299cebb7b843dbd
Phokoos <hulkovskij@gmail.com>
2023-06-22 13:07:03 +0300

    Saved progres

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
+import { combineReducers } from "redux";
 import { counterReducer } from "./counter/counterSlice";
 
-export const reducer = {
+export const reducer = combineReducers({
 	counter: counterReducer,
-}
+}) 
